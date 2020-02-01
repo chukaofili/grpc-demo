ceaa9a237aa34d34c54019c5124389139217da48
Chuka Ofili <ofili@microsmart.tk>
2020-02-01 16:48:19 +0100

    Added both codebases for server & client

--- server/src/server.js
+++ server/src/server.js
@@ -1,34 +1,64 @@
-const url = require('url');
+const { name, version } = require('./package.json');
+const environment = process.env.NODE_ENV || 'development';
+const path = require('path');
 const grpc = require('grpc');
+const pino = require('pino');
+const protoLoader = require('@grpc/proto-loader');
+const { getDistance } = require('geolib');
 
-const proto = grpc.load('interface.proto');
+const SVC_PROTO_PATH = path.join(__dirname, './proto/geoservice.proto');
+const HEALTH_PROTO_PATH = path.join(__dirname, './proto/health/v1/health.proto');
 
-const GeoService = grpc.buildServer([proto.geo.GeoService.service]);
+const PORT = process.env.PORT || 50051;
 
-const server = new GeoService({
-	'geo.GeoService': {
-		distanceBetween: function(call, callback) {
-			callback(null, getDistance(call.request));
-		}
-	}
-});
+const healthProto = _loadProto(HEALTH_PROTO_PATH).grpc.health.v1;
+const { geoservice } = _loadProto(SVC_PROTO_PATH);
 
-function getDistance(points){
-	return distance(points.origin.lat, points.origin.lng, points.destination.lat, points.destination.lng);
+const logger = pino({
+  name,
+  messageKey: 'message',
+  changeLevelName: 'severity',
+  useLevelLabels: true
+}).child({ version, environment });
+
+/**
+ * Helper function that loads a protobuf file.
+ */
+function _loadProto(path) {
+  const packageDefinition = protoLoader.loadSync(path, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
+  );
+  return grpc.loadPackageDefinition(packageDefinition);
 }
 
-server.bind('0.0.0.0:50051');
-server.listen();
-
-
-// From http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
-// User: Salvador Dali - http://stackoverflow.com/users/1090562/salvador-dali
-function distance(lat1,lon1,lat2,lon2) {
-	const R = 6371; // Radius of the earth in km
-	const a =
-		0.5 - Math.cos((lat2 - lat1) * Math.PI / 180)/2 +
-		Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-		(1 - Math.cos((lon2 - lon1) * Math.PI / 180))/2;
-	return R * 2 * Math.asin(Math.sqrt(a));
+/**
+ * Endpoint for health checks
+ */
+function check(call, callback) {
+  callback(null, { status: 'SERVING' });
 }
-////////////////////////////////////
+
+/**
+ * Endpoint for calculating the distance between to points
+ */
+function distanceBetween(call, callback) {
+  const { origin, destination } = call.request
+  const distance = getDistance(origin, destination);
+  callback(null, { distance });
+}
+
+function main() {
+  logger.info(`Starting gRPC server on port ${PORT}...`);
+  const server = new grpc.Server();
+  server.addService(geoservice.GeoService.service, { distanceBetween });
+  server.addService(healthProto.Health.service, { check });
+  server.bind(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure());
+  server.start();
+}
+
+main();
